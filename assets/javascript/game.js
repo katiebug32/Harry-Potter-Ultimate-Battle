2f30f6d43db40e373cc9c57a6de57e8e99fb13e1
katiebug32 <katiebug32@gmail.com>
2019-01-03 22:48:48 -0600

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,48 @@
 //variables//
+var selectedFighterID = null;
+var selectedOpponentID = null;
 var selectedFighter = null;
 var selectedOpponent = null;
-var characters = {
-    "Harry": {
-        healthPoints: 100,
-        attackPower: 6,
-        counterAttack: 20,
-    },
-    Draco: {
-        healthPoints: 60,
-        attackPower: 11,
-        counterAttack: 20,
-    },
-    Snape: {
-        healthPoints: 125,
-        attackPower: 4,
-        counterAttack: 15,
-    },
-    Hermione: {
-        healthPoints: 85,
-        attackPower: 8,
-        counterAttack: 20,
-    },
-    Voldemort: {
-        healthPoints: 150,
-        attackPower: 3,
-        counterAttack: 11,
-    },
-}
+var defeatCount = 0;
 
-var harry = $("#harry").attr(characters.Harry);
-console.log(harry);
+
+function getCharecters() {
+    return {
+        Harry: {
+            healthPoints: 100,
+            attackPower: 8,
+            initialAttackPower: 8,
+            counterAttack: 17,
+        },
+        Snape: {
+            healthPoints: 125,
+            attackPower: 5,
+            initialAttackPower: 5,
+            counterAttack: 12,
+        },
+        Draco: {
+            healthPoints: 60,
+            attackPower: 11,
+            initialAttackPower: 12,
+            counterAttack: 25,
+        },
+        Hermione: {
+            healthPoints: 85,
+            attackPower: 10,
+            initialAttackPower: 10,
+            counterAttack: 23,
+        },
+        Voldemort: {
+            healthPoints: 150,
+            attackPower: 4,
+            initialAttackPower: 4,
+            counterAttack: 10,
+        }
+    }
+}
+var characters = getCharecters();
+
+
 
 function updateHP() {
     $("#harryHP").text(characters.Harry.healthPoints);
@@ -44,31 +56,72 @@ function updateHP() {
 
 
 //game start function//
+
 updateHP();
 $("#playText").append("Choose a character:");
 
 
 //click events//
 
-
 $(".card").click(function (selectCharacter) {
-    if (selectedFighter === null) {
-        $(this).appendTo("#playerChar");
-        selectedFighter = $(this).attr("id");
+    $("#battleText1").text("");
+    $("#battleText2").text("");
+    if (selectedFighterID === null) {
+        $(this).appendTo("#playerChar"); //moves player down
+        selectedFighterID = $(this).attr("id"); //gets id of the selected card
+        // selectedFighter = $(this);
+        // var fighterId = $(this).attr('id');
+        // console.log(fighterId);
+        selectedFighter = characters[selectedFighterID]; //gets object data for that corresponding card
         console.log(selectedFighter);
-        $("#playText").replaceWith("<h2>Choose an opponent:</h2>");
-        $("#vs").append("<h2>VS</h2>");
+       
+        $("#playText").text("Choose an opponent:");
+        $("#vs").append("<h2>VS</h2>"); // creates space and VS text between battle cards
     }
-    else if (selectedOpponent === null) {
-        $(this).appendTo("#opponentChar");
-        selectedOpponent = $(this).attr("id");
+    else if (selectedOpponentID === null) {
+        $(this).appendTo("#opponentChar"); //moves opponent down
+        selectedOpponentID = $(this).attr("id"); //gets id of the selected card
+        selectedOpponent = characters[selectedOpponentID];  //gets object data for that corresponding card
         console.log(selectedOpponent);
-        $(".btn").css("display", "flex");
+       
+        $(".btn").css("display", "inline");
+        $("#playText").text("Ready, set, battle!");
     }
 });
 
-// $("#btn").click(function (battle) {
-//     selectedFighter 
-//   })
+$("#fightButton").click(function (battle) {
+    $("#battleText1").text("");
+    $("#battleText2").text("");
+    if ((selectedFighter.healthPoints > 0) && (selectedOpponent.healthPoints > 0)) {
 
-// Do I need to link the car id's to the respective char info in the object? 
+        selectedOpponent.healthPoints = selectedOpponent.healthPoints - selectedFighter.attackPower;
+        $("#battleText1").text("You attacked " + selectedOpponentID + " for " + selectedFighter.attackPower + " damage.");
+        if (selectedOpponent.healthPoints < 1) {
+            $("#" + selectedOpponentID).css("visibility", "hidden").appendTo("#selectChar");
+            defeatCount++;
+            if (defeatCount === 4) {
+                $("#battleText1").text("You won! Would you like to play again?");
+                //reset everything here!!!//
+            }
+            else {
+                $("#battleText2").text("You defeated " + selectedOpponentID + ". Select another opponent.");
+                selectedOpponentID = null;
+            }
+        }
+        else {
+            selectedFighter.healthPoints = selectedFighter.healthPoints - selectedOpponent.counterAttack;
+            $("#battleText2").text(selectedOpponentID + " attacked you for " + selectedOpponent.counterAttack + " damage.");
+        }
+        updateHP();
+        selectedFighter.attackPower = selectedFighter.attackPower + selectedFighter.initialAttackPower;
+        // selectedFighter.healthPoints = sum();
+        // console.log(selectedFighter.healthPoints); 
+    }
+   if (selectedFighter.healthPoints < 1) {
+       $("#battleText1").text("Game Over. " + selectedOpponentID + " beat you this time. Want to play again?");
+       $("#battleText2").text("");
+
+
+   }
+  })
+
